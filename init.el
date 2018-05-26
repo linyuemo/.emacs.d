9b36bf7ce4e62793423b82d69594d593da1bb08a
‘linyuemo’ <a819337877@163.com>
2018-05-26 11:36:17 +0800

    Based purcell build myself emacs .emacs.d init config.
    1. set f2 key to open init file quickly.
    2. set coding text enviroment , but i don't know whether them work.
    3. delete some requires that i don't needed.

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+;;; package --- Summary
+;;; Commentary:
 ;; -*- lexical-binding: t -*-
 
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
+;;; Code:
 (let ((minver "24.3"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
@@ -14,6 +17,51 @@
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Open config file fastly
+;;----------------------------------------------------------------------------
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+
+;;;; 设置编辑环境
+;; 设置为中文简体语言环境
+(set-language-environment 'Chinese-GB)
+;; 设置emacs 使用 utf-8
+(setq locale-coding-system 'utf-8)
+;; 设置键盘输入时的字符编码
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+;; 文件默认保存为 utf-8
+(set-buffer-file-coding-system 'utf-8)
+(set-default buffer-file-coding-system 'utf8)
+(set-default-coding-systems 'utf-8)
+;; 解决粘贴中文出现乱码的问题
+(set-clipboard-coding-system 'utf-8)
+;; 终端中文乱码
+(set-terminal-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+;; 解决文件目录的中文名乱码
+(setq-default pathname-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
+;; 解决 Shell Mode(cmd) 下中文乱码问题 but it don't work!!
+(defun change-shell-mode-coding ()
+
+  (progn
+    (set-terminal-coding-system 'gbk)
+    (set-keyboard-coding-system 'gbk)
+    (set-selection-coding-system 'gbk)
+    (set-buffer-file-coding-system 'gbk)
+    (set-file-name-coding-system 'gbk)
+    (modify-coding-system-alist 'process "*" 'gbk)
+    (set-buffer-process-coding-system 'gbk 'gbk)
+    (set-file-name-coding-system 'gbk)))
+
+
 
 ;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
@@ -49,70 +97,60 @@
 (require-package 'scratch)
 (require-package 'command-log-mode)
 
-(require 'init-frame-hooks)
-(require 'init-xterm)
-(require 'init-themes)
+(require 'init-frame-hooks) ;;hooks
+(require 'init-xterm)  ;; mouse
+(require 'init-themes) ;; themes
 (require 'init-osx-keys)
-(require 'init-gui-frames)
-(require 'init-dired)
+(require 'init-gui-frames) ;; gui
+(require 'init-dired)  ;; dired
 (require 'init-isearch)
 (require 'init-grep)
 (require 'init-uniquify)
 (require 'init-ibuffer)
-(require 'init-flycheck)
+;;(require 'init-flycheck)
 
+;; Enhance emacs foundation function
 (require 'init-recentf)
-(require 'init-smex)
-(require 'init-ivy)
-(require 'init-hippie-expand)
+(require 'init-smex) ;;enhance M-x
+(require 'init-ivy)  ;;enhance C-
+(require 'init-hippie-expand) ;;Automatic completion
 (require 'init-company)
-(require 'init-windows)
-(require 'init-sessions)
+(require 'init-windows) ;; Windows operation
+(require 'init-sessions) ;; Session time
 (require 'init-fonts)
-(require 'init-mmm)
+(require 'init-mmm) ;; Multiple major modes
 
+;; Powerful edit tools
 (require 'init-editing-utils)
 (require 'init-whitespace)
 
+;; Git tools
 (require 'init-vc)
 (require 'init-darcs)
 (require 'init-git)
 (require 'init-github)
 
+;; ??
 (require 'init-projectile)
 
-(require 'init-compile)
-;;(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
-(require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-php)
-(require 'init-org)
-(require 'init-nxml)
-(require 'init-html)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-http)
-(require 'init-python-mode)
-(require 'init-haskell)
-(require 'init-elm)
-(require 'init-purescript)
-(require 'init-ruby-mode)
-(require 'init-rails)
-(require 'init-sql)
-(require 'init-rust)
-(require 'init-toml)
-(require 'init-yaml)
-(require 'init-docker)
-(require 'init-terraform)
+;;(require 'init-compile)
+;;(require 'init-markdown)
+;;(require 'init-csv)
+;;(require 'init-org)
+;;(require 'init-nxml)
+;;(require 'init-html)
+;;(require 'init-css)
+;;(require 'init-http)
 
+;; Usually using development mode
+(require 'init-python-mode)
+(require 'init-ruby-mode)
+
+
+;; Lisp development enviroment
 (require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
-(require 'init-clojure)
-(require 'init-clojure-cider)
 (require 'init-common-lisp)
 
 (when *spell-check-support-enabled*
@@ -170,4 +208,4 @@
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
-;; End:
+;;; init.el ends here
